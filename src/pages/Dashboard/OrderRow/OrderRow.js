53e786deff54a195b3f40cc8302d79b34e6e10d3
srabon0 <srabonemam4@gmail.com>
2023-01-30 15:47:42 +0600

    initiate payment order

--- src/pages/Dashboard/OrderRow/OrderRow.js
+++ src/pages/Dashboard/OrderRow/OrderRow.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 
+const OrderRow = ({ order, index, modalOrder }) => {
+  const initiatePayment = (id) => {
 
-
-const OrderRow = ({ order, index,modalOrder }) => {
+    const url = "http://localhost:5000/payment/ssl-request/"+id
+    window.location.replace(url);
+  };
   const dispatch = useDispatch();
-  const {setModalOrder} = modalOrder
-  const { _id, name, email, subtotal,order_date } = order;
+  const { setModalOrder } = modalOrder;
+  const { _id, name, email, subtotal, order_date } = order;
   return (
     <tr>
       <td>{index + 1}</td>
-      <td >
-        <label onClick={()=>setModalOrder(order)} htmlFor="ordermodal" className="bg-info text-black text-sm rounded-sm underline px-1 block">{_id}</label>
-        <p className="badge badge-ghost badge-sm">Date: {order_date.split("T")[0]}</p>
+      <td>
+        <label
+          onClick={() => setModalOrder(order)}
+          htmlFor="ordermodal"
+          className="bg-info text-black text-sm rounded-sm underline px-1 block"
+        >
+          {_id}
+        </label>
+        <p className="badge badge-ghost badge-sm">
+          Date: {order_date.split("T")[0]}
+        </p>
       </td>
       <td>
         {name}
@@ -21,13 +32,20 @@ const OrderRow = ({ order, index,modalOrder }) => {
       </td>
       <td>{subtotal}</td>
       <td>
-      <button className="btn btn-success text-white btn-sm">Unpaid</button>
+        <button
+          onClick={() => {
+            initiatePayment(_id);
+          }}
+          className="btn btn-success text-white btn-sm"
+        >
+          Pay
+        </button>
+
       </td>
 
       <th>
         <button className="btn btn-error text-white btn-sm">Cancel</button>
       </th>
-      
     </tr>
   );
 };
